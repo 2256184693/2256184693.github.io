e32fa231648b809fe0472e306605a7db4de153cb
sh <shwan_88@126.com>
2021-11-23 02:51:54 +0800

    leecode: 两数相除

--- leeCode/29.两数相除.js
+++ leeCode/29.两数相除.js
@@ -42,10 +42,60 @@ const divide = function (dividend, divisor) {
     if (a >>> i >= b) {
       res += 1 << i;
       a -= b << i;
-      console.log(`res = ${res}, a = ${a}`);
     }
   }
 
   return isNegative ? -res : res;
 };
+
+// 位运算 超时？？
+const divide2 = function (dividend, divisor) {
+  if (dividend === 0) {
+    return 0;
+  }
+
+  const MAX = 2 ** 31 - 1;
+
+  const MIN = -(2 ** 31);
+
+  if (dividend === MIN && divisor === -1) {
+    return MAX;
+  }
+
+  if (dividend === MIN && divisor === 1) {
+    return MIN;
+  }
+
+  const isNegative = (dividend ^ divisor) < 0;
+
+  let [a, b] = [Math.abs(dividend), Math.abs(divisor)];
+
+  let res = 0;
+
+  for (let i = 31; i >= 0; i--) {
+    if (a >>> i >= b) {
+      res += 1 << i;
+      a -= b << i;
+    }
+  }
+
+  return isNegative ? -res : res;
+};
+
+// 位运算 超时？？
+const divide3 = function (dividend, divisor) {
+  const isNegative = (dividend ^ divisor) < 0;
+
+  let [a, b] = [Math.abs(dividend), Math.abs(divisor)];
+
+  let res = 0;
+
+  while (a <= b) {
+    a -= b;
+    res++;
+  }
+
+  return isNegative ? -res : res;
+};
+
 // @lc code=end
